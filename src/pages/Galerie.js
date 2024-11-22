42f2f6fb37cc22c085812f22f871467949e6b6fd
Giu1447 <giulian.mazzeo@lernende.bbw.ch>
2024-11-22 14:46:20 +0100

    video and gallery

--- src/pages/Galerie.js
+++ src/pages/Galerie.js
@@ -1,56 +1,95 @@
-import React, { useState } from 'react';
+import React from 'react';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Galerie = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-
   const images = [
-    "https://placehold.co/600x400/orange/white",
-    "https://placehold.co/600x400/blue/white",
-    "https://placehold.co/600x400/green/white",
-    "https://placehold.co/600x400/purple/white",
-    "https://placehold.co/600x400/yellow/white",
+    {
+      src: "https://placehold.co/600x400/orange/white",
+      text: "Bild 1",
+    },
+    {
+      src: "https://placehold.co/600x400/red/white",
+      text: "Bild 2",
+    },
+    {
+      src: "https://placehold.co/600x400/blue/white",
+      text: "Bild 3",
+    },
+    {
+      src: "https://placehold.co/600x400/green/white",
+      text: "Bild 4",
+    },
+    {
+      src: "https://placehold.co/600x400/purple/white",
+      text: "Bild 5",
+    },
+    {
+      src: "https://placehold.co/600x400/yellow/white",
+      text: "Bild 6",
+    }
   ];
 
-  const goToPrevious = () => {
-    setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+  // Define CustomArrow components
+  const CustomPrevArrow = ({ onClick }) => {
+    return (
+        <button
+            className="absolute top-1/2 left-[-100px] transform -translate-y-1/2 bg-gray-700 bg-opacity-80 p-3 rounded-full hover:bg-opacity-90 text-white"
+            onClick={onClick}
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+               stroke="currentColor" className="w-8 h-8">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
+          </svg>
+        </button>
+    );
   };
 
-  const goToNext = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+  const CustomNextArrow = ({ onClick }) => {
+    return (
+        <button
+            className="absolute top-1/2 right-[-100px] transform -translate-y-1/2 bg-gray-700 bg-opacity-80 p-3 rounded-full hover:bg-opacity-90 text-white"
+            onClick={onClick}
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+               stroke="currentColor" className="w-8 h-8">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5l7.5 7.5m0 0-7.5 7.5M21 12H3" />
+          </svg>
+        </button>
+    );
+  };
+
+  // Slick Slider settings
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 900,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: true,
+    prevArrow: <CustomPrevArrow />,
+    nextArrow: <CustomNextArrow />,
   };
 
   return (
       <div className="container mx-auto p-4 bg-primary">
         <div className="flex flex-col items-center">
-          <p className="text-secondary mb-8">Stöbern Sie durch unsere beeindruckende Bildersammlung.</p>
-
-          <div className="relative w-full max-w-4xl h-96">
-            <div className="overflow-hidden rounded-lg h-full">
-              <img
-                  src={images[currentIndex]}
-                  alt={`Image ${currentIndex + 1}`}
-                  className="w-full h-full object-cover transition-transform duration-500"
-              />
-            </div>
-
-            {/* Linker Pfeil */}
-            <button
-                className="absolute top-1/2 left-[-100px] transform -translate-y-1/2 bg-gray-700 bg-opacity-80 p-3 rounded-full hover:bg-opacity-90 text-white"
-                onClick={goToPrevious}
-            >
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
-              </svg>
-            </button>
-
-            <button
-                className="absolute top-1/2 right-[-100px] transform -translate-y-1/2 bg-gray-700 bg-opacity-80 p-3 rounded-full hover:bg-opacity-90 text-white"
-                onClick={goToNext}
-            >
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5l7.5 7.5m0 0-7.5 7.5M21 12H3" />
-              </svg>
-            </button>
+          <div className="relative w-full max-w-4xl">
+            <Slider {...settings}>
+              {images.map((image, index) => (
+                  <div key={index} className="overflow-hidden ">
+                    <p className="mt-4 text-secondary text-lg mb-4 text-center mb4">{image.text}</p>
+                    <img
+                        src={image.src}
+                        alt={`Image ${index + 1}`}
+                        className="w-full h-[500px] object-cover rounded-lg"
+                    />
+                  </div>
+              ))}
+            </Slider>
           </div>
         </div>
       </div>
--- src/pages/Videos.js
+++ src/pages/Videos.js
@@ -3,12 +3,52 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+const CustomPrevArrow = ({ onClick }) => {
+    return (
+        <button
+            className="absolute top-1/2 left-[-100px] transform -translate-y-1/2 bg-gray-700 bg-opacity-80 p-3 rounded-full hover:bg-opacity-90 text-white"
+            onClick={onClick}
+        >
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+                 stroke="currentColor" className="w-8 h-8">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"/>
+            </svg>
+        </button>
+    );
+};
+
+const CustomNextArrow = ({onClick}) => {
+    return (
+        <button
+            className="absolute top-1/2 right-[-100px] transform -translate-y-1/2 bg-gray-700 bg-opacity-80 p-3 rounded-full hover:bg-opacity-90 text-white"
+            onClick={onClick}
+        >
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+                 stroke="currentColor" className="w-8 h-8">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5l7.5 7.5m0 0-7.5 7.5M21 12H3"/>
+            </svg>
+        </button>
+    );
+};
+
 const Videos = () => {
-    const videoSources = [
-        "https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
-        "https://storage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
-        "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
-        "https://storage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4"
+    const videos = [
+        {
+            src: "https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+            text: "Das Abenteuer von Big Buck Bunny!"
+        },
+        {
+            src: "https://storage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
+            text: "Eine surreale Reise in Elephants Dream."
+        },
+        {
+            src: "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
+            text: "Für größere Flammen – Feuer frei!"
+        },
+        {
+            src: "https://storage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4",
+            text: "Tears of Steel – Eine epische Sci-Fi-Erzählung."
+        },
     ];
 
     const settings = {
@@ -17,22 +57,25 @@ const Videos = () => {
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1,
-        autoplay: true,
+        autoplay: false,
         autoplaySpeed: 3000,
         arrows: true,
+        prevArrow: <CustomPrevArrow />,
+        nextArrow: <CustomNextArrow />,
     };
 
     return (
-        <div className="container mx-auto p-4 bg-primary">
+        <div className="container mx-auto p-4 bg-primary text-center">
             <div className="max-w-4xl mx-auto">
                 <Slider {...settings}>
-                    {videoSources.map((src, index) => (
+                    {videos.map((video, index) => (
                         <div key={index}>
+                            <p className="mt-4 text-secondary text-lg mb-4">{video.text}</p>
                             <video
                                 controls
-                                className="w-full h-auto rounded-lg border-2 border-accent"
+                                className="w-full h-[500px] object-cover rounded-lg border-2 border-accent mb-4"
                             >
-                                <source src={src} type="video/mp4" />
+                                <source src={video.src} type="video/mp4"/>
                                 Ihr Browser unterstützt das Video-Tag nicht.
                             </video>
                         </div>
